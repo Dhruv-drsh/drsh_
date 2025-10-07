dca21976a85fe7af6a67e4a5898267a92fefe3ee
Dhruv-drsh <dhruvarcade27@gmail.com>
2025-10-07 19:01:45 +0530

    Delete frontend directory

--- frontend/src/utils.js
+++
@@ -1,7 +0,0 @@
-import { clsx } from "clsx";
-import { twMerge } from "tailwind-merge";
-
-// This function merges multiple class names and resolves Tailwind conflicts!
-export function cn(...inputs) {
-  return twMerge(clsx(inputs));
-}
